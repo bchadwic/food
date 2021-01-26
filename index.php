991ce60ab8d53a93dc9efcfc929c06d074bb11ad
bchadwic <bchadwick@mail.greenriver.edu>
2021-01-26 11:55:03 -0800

    Added a controller to the site

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+//Turn on error reporting -- this is critical!
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+$f3->route('GET /', function() {
+    // echo "Adding index page";
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+
+// Define a "breakfast" route
+$f3->route('GET /breakfast', function() {
+    echo "Breakfast";
+});
+
+//Run fat free
+$f3->run();
+
