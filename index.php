30d9fa831ab21e37e7aab96b3c87449eee3ba72e
bchadwic <bchadwick@mail.greenriver.edu>
2021-01-26 12:48:58 -0800

    Added additional routes dependant on parameters

--- index.php
+++ index.php
@@ -34,6 +34,44 @@ $f3->route('GET /lunch/sandwich', function() {
     echo $view->render('views/sandwich.html');
 });
 
+$f3->route('GET /@first/@last', function($f3, $params) {
+    echo "Hello, " .$params['first']." ".$params['last'];
+
+});
+
+$f3->route('GET /breakfast/@item', function($f3, $params) {
+    $item = $params['item'];
+    $menu = array('eggs', 'waffles', 'pancakes');
+    if(in_array($menu, $menu)){
+        switch($item) {
+            case 'eggs' :
+                $view = new Template();
+                echo $view->render('views/eggs.html');
+                break;
+            case 'pancakes':
+                echo "Swedish or American?";
+                break;
+            case 'waffles' :
+               $f3->reroute("http://thewafflehouse.com");
+                break;
+            default:
+                $f3->error(404);
+        }
+        echo "We serve $item";
+        // for example if the item displayed is in the array, we go to a certain view
+
+        //$f3->route('GET /lunch/item', function() {
+        //    $view = new Template();
+        //    echo $view->render('views/item.html');
+        //});
+
+    } else {
+        echo "Sorry we don't serve $item";
+    }
+//    $view = new Template();
+//    echo $view->render('views/.html');
+});
+
 
 
 //Run fat free
